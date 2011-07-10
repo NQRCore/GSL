b7013254eeeb8ad040ead1bec096fa4bef5d6779
Jay Emerson <john.emerson@yale.edu>
2011-07-10 19:28:41 -0400

    Simple libgsl call example now working!

--- src/GSL/Runtime.pm
+++ src/GSL/Runtime.pm
@@ -12,19 +12,27 @@ sub say(*@args) {
 
 # TEST: Function: double gsl_ran_gaussian_pdf (double x, double sigma)
 # See SCRATCH file for notes.
+    #.local pmc gsl_ran_gaussian_pdf
+    #gsl_ran_gaussian_pdf = dlfunc libgsl, "gsl_ran_gaussian_pdf", "ddd"
+    #ans = gsl_ran_gaussian_pdf(xval, sigma)
+# This didn't work because of a thunk problem.  Revisit NCI stuff.
+
+# Works with a simpler function from the gsl library:
 sub test(*@args) {
   my $ans;
   ans := Q:PIR {
-    .local pmc libgsl, gsl_ran_gaussian_pdf
+    .local pmc libgsl, gsl_ran_ugaussian_pdf
     .local num ans, xval, sigma
-    xval = 0.0
+    xval = 0.5
     sigma = 1.0
     libgsl = loadlib "libgsl"
     if libgsl goto HAVELIBRARY
       say 'Could not load the library'
     HAVELIBRARY:
-    gsl_ran_gaussian_pdf = dlfunc libgsl, "gsl_ran_gaussian_pdf", "ddd"
-    ans = gsl_ran_gaussian_pdf(xval, sigma)
+    gsl_ran_ugaussian_pdf = dlfunc libgsl, "gsl_ran_ugaussian_pdf", "dd"
+    ans = gsl_ran_ugaussian_pdf(xval)
+    say "Hello, maybe succeeded!"
+    say ans
     %r = box ans
   };
   print($ans);
